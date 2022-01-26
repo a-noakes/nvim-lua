518b941ab6c0acfd3f9b8593cb6569751f27fd44
alexis <alexis.noakes@sas.se>
2022-01-26 23:14:20 +0100

    added additional colorschemes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,8 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use "morhetz/gruvbox"
+  use "altercation/vim-colors-solarized"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
