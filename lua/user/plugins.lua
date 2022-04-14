ed4fdd2a4f892b9f35639ae0594155a04e237221
Alexis Noakes <alexis.noakes@sas.se>
2022-04-14 18:29:18 +0200

    nvim-cmp update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,7 +81,6 @@ return packer.startup(function(use)
   -- use "metakirby5/codi.vim"
   use "nyngwang/NeoZoom.lua"
   use "SmiteshP/nvim-gps"
-  use "karoliskoncevicius/vim-sendtowindow" -- Send text to windows
   use { "michaelb/sniprun", run = "bash ./install.sh" }
   use {
 
@@ -93,14 +92,16 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use "folke/tokyonight.nvim"
-  use "ishan9299/nvim-solarized-lua"
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "rose-pine/neovim"
   -- use "rebelot/kanagawa.nvim"
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
+  -- use "hrsh7th/nvim-cmp" -- The completion plugin
+    use { 'hrsh7th/nvim-cmp',
+      commit = "dbc72290295cfc63075dab9ea635260d2b72f2e5",
+    }
   use "hrsh7th/cmp-buffer" -- buffer completions
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
